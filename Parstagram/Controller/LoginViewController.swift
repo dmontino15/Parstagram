3f9c631b5e36ce18afb2238499864587046939d2
Daniella Montinola <dmonti15@gmail.com>
2019-11-15 00:25:02 -0600

    Completed sign up and login setup

--- Parstagram/Controller/LoginViewController.swift
+++ Parstagram/Controller/LoginViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func onSignInBtn(_ sender: UIButton) {
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUpBtn(_ sender: UIButton) {
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+
+    }
+    
+    // MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
