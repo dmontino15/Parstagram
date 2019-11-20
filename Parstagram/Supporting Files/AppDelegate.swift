abed16a5a7f900ec4a5b4ac6c656a4940284d3d2
Daniella Montinola <dmonti15@gmail.com>
2019-11-19 22:01:58 -0600

    Staying logged in

--- Parstagram/Supporting Files/AppDelegate.swift
+++ Parstagram/Supporting Files/AppDelegate.swift
@@ -22,6 +22,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://ancient-spire-83140.herokuapp.com/parse"
             })
         )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
+        
         return true
     }
 
